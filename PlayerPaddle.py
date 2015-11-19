a3b422694aed35712670477b9333bc4db93bc5fe
Aaron Le <Aaron.Le@kearsarge.org>
2015-11-19 12:34:44 -0500

    Sweg

--- PlayerPaddle.py
+++ PlayerPaddle.py
@@ -1,1 +1,24 @@
+import sys, pygame, math
+from Ball import Ball
 
+class PlayerPaddle(Ball):
+    def __init__(self, image, maxSpeed, pos = [0,0]):
+        Ball.__init__(self, image, [0,0], pos)
+        
+        self.maxSpeedx = maxSpeed[0]
+        self.maxSpeedy = maxSpeed[1]
+
+    def collideScreen(self, size):
+        width = size[0]
+        height = size[1]
+        
+    def go(self, direction):
+        if direction == "up":
+            self.speedy = -self.maxSpeedy
+        elif direction == "down":
+            self.speedy = self.maxSpeedy
+        
+        if direction == "stop up":
+            self.speedy = 0
+        elif direction == "stop down":
+            self.speedy = 0
