01c9ef18ccd7454ec535b1b8d692aaa543c95bed
Aaron Le <Aaron.Le@kearsarge.org>
2015-11-20 12:26:26 -0500

    Connor is gay

--- EpicPong.py
+++ EpicPong.py
@@ -13,7 +13,8 @@ bgColor = r,b,g = 120,0,56
 
 screen = pygame.display.set_mode(size)
 
-player = PlayerPaddle( [width/2, height/2], "prainbow.png")
+player1 = PlayerPaddle( [width/2, height/2], "pRainbow.png")
+#player2 = PlayerPaddle( [width/2, height/2], "pRainbow.png")
 
 
 while True:
@@ -21,6 +22,17 @@ while True:
         if event.type == pygame.QUIT: 
             sys.exit()
         
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.go("up")
+            elif event.key == pygame.K_DOWN:
+                player.go("down")
+           
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player.go("stop up")
+            elif event.key == pygame.K_DOWN:
+                player.go("stop down")
         
         bgColor = r,b,g
     screen.fill(bgColor)
