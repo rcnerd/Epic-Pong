db8bf33dd16ba73db0385e31dac81dc7f95f4e84
Aaron Le <Aaron.Le@kearsarge.org>
2015-12-03 12:46:04 -0500

    Hello

--- Ball.py
+++ Ball.py
@@ -15,19 +15,21 @@ class Ball():
         self.rect = self.image.get_rect()
         self.radius = self.rect.width/2 - 2
         
+        self.frame = 0
+        self.maxFrame = len(self.images)-1
+        self.timer = 0
+        self.timerMax = .15 * 60
+        
         self.didBounceX = False
         self.didBounceY = False
-        
-        #self.frame = 0
-        #self.maxFrame = len(self.images)-1
-        #self.timer = 0
-        #self.timerMax = .05 * 60
-        
+
         self.rect = self.rect.move(pos)
 
     def move(self):
         self.speed = [self.speedx, self.speedy]
         self.rect = self.rect.move(self.speed)
+        self.didBounceX = False
+        self.didBounceY = False
 
     def collideScreen(self, size):
         width = size[0]
@@ -36,15 +38,35 @@ class Ball():
         if self.rect.top > height or self.rect.bottom < height:
             if not self.didBounceX:
                 self.speedx = 0
-        
+                
+        if not self.didBounceX:
+            if self.rect.left < 0 or self.rect.right > width:
+                self.speedx = -self.speedx
                 self.didBounceX = True
+                self.move()
+        if not self.didBounceY:
+            if self.rect.top < 0 or self.rect.bottom > height:
+                self.speedy = -self.speedy
+                selfdidBounceY = True
+                self.move()
         
         return True
         
     def update(self, size):
         self.move()
-        #self.animate()
+        self.animate()
         self.collideScreen(size)
+        
+    def animate(self):
+        if self.timer < self.timerMax:
+            self.timer += 1
+        else:
+            self.timer = 0
+            if self.frame < self.maxFrame:
+                self.frame += 1
+            else:
+                self.frame = 0
+        self.image = self.images[self.frame]
         
     def collideBall(self, other):
         if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
