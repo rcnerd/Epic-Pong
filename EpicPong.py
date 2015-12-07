f4c9fd3d406d915c5e4488ba97341735140364e8
Aaron Le <Aaron.Le@kearsarge.org>
2015-12-07 12:19:46 -0500

    Paddles working

--- EpicPong.py
+++ EpicPong.py
@@ -9,7 +9,7 @@ width = 900
 height = 700
 size = width, height
 
-bgColor = r,b,g = 255,0,255
+bgColor = r,b,g = 103,255,230
 
 screen = pygame.display.set_mode(size)
 
@@ -17,8 +17,8 @@ balls = []
 ballTimer = 0
 ballTimerMax = .1 * 60
 
-player = PlayerPaddle( ["pRainbow.png"], [10,10], [850, 600])
-player2 = PlayerPaddle( ["player2.png"], [10,10], [0,0])
+player = PlayerPaddle( ["pRainbow.png"], [10,10], [825, 600])
+player2 = PlayerPaddle( ["player2.png"], [10,10], [25,0])
 
 ball = Ball(["BlackBall.png"], [6,6], [300,400])
 
@@ -52,7 +52,7 @@ while True:
     ballTimer += 1
     if ballTimer >= ballTimerMax:
         ballTimer = 0
-        if len(balls) < 2:
+        if len(balls) < 1:
             #ballSpeed = random.randint(-5, 5)
             #ballPos = random.randint(100, width-100)
             balls += [Ball(["BlackBall.png",
@@ -73,6 +73,8 @@ while True:
         player.collideBall(first)
         if first.collideBall(player):
             first.collidePaddle(player)
+        elif first.collideBall(player2):
+            first.collidePaddle(player2)
         else:
             for second in balls:
                 if first != second:
