66cf22e3755b7d3dbaa23af12d8171ffe59c273d
MrsJackson <Alyissa.jackson@kearsarge.org>
2015-12-09 11:48:29 -0500

    yur

--- EpicPong.py
+++ EpicPong.py
@@ -17,8 +17,8 @@ balls = []
 ballTimer = 0
 ballTimerMax = .1 * 60
 
-player = PlayerPaddle( ["Pics/player/player.png"], [10,10], [885, 300])
-player2 = PlayerPaddle( ["Pics/PLayer/player2.png"], [10,10], [5, 300])
+player = PlayerPaddle( ["Pics/Player/pRainbow.png"], [10,10], [850, 600])
+player2 = PlayerPaddle( ["Pics/Player/player2.png"], [10,10], [0, 0])
 
 while True:
     for event in pygame.event.get():
