f0f168e85067d4f8653ac54117d4042da873c46b
MrsJackson <Alyissa.jackson@kearsarge.org>
2015-12-14 12:18:57 -0500

    score file finished

--- Score.py
+++ Score.py
@@ -1,1 +1,23 @@
+import sys, pygame, math
+from ball import Ball
 
+class Score():
+    def __init__(self, pos = [0,0]):
+        self.score = 0
+        self.font = pygame.font.Font(None, 36)
+        self.image = self.font.render(str(self.score), 1, (0, 0, 0))
+        self.rect = self.image.get_rect(center = pos)
+        
+    def update():
+        self.image = self.font.render(str(self.score), 1, (0, 0, 0))
+        self.rect = self.image.get_rect(center = self.rect.center)
+        
+    
+    def increase(self, amount=0):
+        self.score += amount
+    
+    def decrease(self, amount=0):
+        self.score -= amount
+        
+    def reset(self, amount=0):
+        self.score = amount
